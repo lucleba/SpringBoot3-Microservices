cfd1c2cb4a4c5a9c274b686ffe6fea05ab1db3bc
LeBaLuc <lebaluc.atk@gmail.com>
2024-09-18 00:33:20 +0700

    1. set bien moi truong cho signer key 2. verify token

--- user-management-service/src/main/java/com/lucle/user_management_service/service/AuthenticationService.java
+++ user-management-service/src/main/java/com/lucle/user_management_service/service/AuthenticationService.java
@@ -1,22 +1,28 @@
 package com.lucle.user_management_service.service;
 
 import com.lucle.user_management_service.dto.request.AuthenticationRequest;
+import com.lucle.user_management_service.dto.request.IntrospectRequest;
 import com.lucle.user_management_service.dto.response.AuthenticationResponse;
+import com.lucle.user_management_service.dto.response.IntrospectResponse;
 import com.lucle.user_management_service.exception.AppException;
 import com.lucle.user_management_service.exception.ErrorCode;
 import com.lucle.user_management_service.repository.UserRepository;
 import com.nimbusds.jose.*;
 import com.nimbusds.jose.crypto.MACSigner;
+import com.nimbusds.jose.crypto.MACVerifier;
 import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.experimental.NonFinal;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.text.ParseException;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
@@ -29,7 +35,27 @@ public class AuthenticationService {
     UserRepository userRepository;
 
     @NonFinal // danh dau de khong inject vao contractor
-    protected  static final String SIGNER_KEY = "OIZpkwRnu0bwgWR1A/Jp4QJeoFCjxLyRzz1MBcnbnhix8c3Io1NJ/qU4kCIV+6sQ";
+    @Value("${jwt.signerKey}")
+    protected String signerKey;
+
+
+    public IntrospectResponse introspect(IntrospectRequest request)
+            throws JOSEException, ParseException {
+        var token = request.getToken();
+
+        JWSVerifier verifier = new MACVerifier(signerKey.getBytes());
+
+        SignedJWT signedJWT = SignedJWT.parse(token);
+
+        Date expiryTime = signedJWT.getJWTClaimsSet().getExpirationTime();
+
+        var verified = signedJWT.verify(verifier);
+
+        return IntrospectResponse.builder()
+                .valid(verified && expiryTime.after(new Date()))
+                .build();
+    }
+
 
     public AuthenticationResponse authenticate(AuthenticationRequest request){
         var user = userRepository.findByUsername(request.getUsername())
@@ -66,7 +92,7 @@ public class AuthenticationService {
         JWSObject jwsObject = new JWSObject(header, payload);
 
         try {
-            jwsObject.sign(new MACSigner(SIGNER_KEY.getBytes()));
+            jwsObject.sign(new MACSigner(signerKey.getBytes()));
             return jwsObject.serialize();
         } catch (JOSEException e) {
             log.error("Can not generate token", e);
