98b978f02efe600c0339fa2edbac5cfe5c647210
LeBaLuc <lebaluc.atk@gmail.com>
2024-09-18 00:12:50 +0700

    1. add thu vien Nimbus JOSE+JWT 2. generate token

--- user-management-service/src/main/java/com/lucle/user_management_service/service/AuthenticationService.java
+++ user-management-service/src/main/java/com/lucle/user_management_service/service/AuthenticationService.java
@@ -1,28 +1,76 @@
 package com.lucle.user_management_service.service;
 
 import com.lucle.user_management_service.dto.request.AuthenticationRequest;
+import com.lucle.user_management_service.dto.response.AuthenticationResponse;
 import com.lucle.user_management_service.exception.AppException;
 import com.lucle.user_management_service.exception.ErrorCode;
 import com.lucle.user_management_service.repository.UserRepository;
+import com.nimbusds.jose.*;
+import com.nimbusds.jose.crypto.MACSigner;
+import com.nimbusds.jwt.JWTClaimsSet;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.experimental.NonFinal;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
+@Slf4j
 @Service
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class AuthenticationService {
     UserRepository userRepository;
 
-    public boolean authenticate(AuthenticationRequest request){
+    @NonFinal // danh dau de khong inject vao contractor
+    protected  static final String SIGNER_KEY = "OIZpkwRnu0bwgWR1A/Jp4QJeoFCjxLyRzz1MBcnbnhix8c3Io1NJ/qU4kCIV+6sQ";
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request){
         var user = userRepository.findByUsername(request.getUsername())
                 .orElseThrow(() -> new AppException(ErrorCode.USER_NOT_EXISTED));
 
         PasswordEncoder passwordEncoder = new BCryptPasswordEncoder(10);
         // passwordEncoder.matches de kiem tra xem password nhap vao co khop voi password trong database khong
-        return passwordEncoder.matches(request.getPassword(), user.getPassword());
+        boolean authenticated = passwordEncoder.matches(request.getPassword(), user.getPassword());
+        if (!authenticated)
+            throw new AppException(ErrorCode.UNAUTHENTICATED);
+        var token = generateToken(user.getUsername());
+
+        return AuthenticationResponse.builder()
+                .authenticated(true)
+                .token(token)
+                .build();
+    }
+
+    private String generateToken(String username){
+        JWSHeader header = new JWSHeader(JWSAlgorithm.HS512);
+
+        JWTClaimsSet jwtClaimsSet = new JWTClaimsSet.Builder()
+                .subject(username)
+                .issuer("lucle.com")
+                .issueTime(new Date())
+                .expirationTime(
+                        new Date(
+                                Instant.now().plus(1, ChronoUnit.HOURS).toEpochMilli()
+                        )
+                )
+                .claim("customClaim", "Custom")
+                .build();
+        Payload payload = new Payload(jwtClaimsSet.toJSONObject());
+        JWSObject jwsObject = new JWSObject(header, payload);
+
+        try {
+            jwsObject.sign(new MACSigner(SIGNER_KEY.getBytes()));
+            return jwsObject.serialize();
+        } catch (JOSEException e) {
+            log.error("Can not generate token", e);
+            throw new RuntimeException(e);
+        }
     }
 }
